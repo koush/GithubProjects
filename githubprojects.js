d4342eb9ddc6147c349f4eb846a2b693e6465bb9
Koushik Dutta <koush@Koushik-MacBook.local>
2009-05-12 16:06:35 -0700

    initial commit

---
+++ githubprojects.js
@@ -0,0 +1,28 @@
+var GithubProjects = {};
+
+GithubProjects.showImage = function(img) {
+	img.style.visibility = 'visible';
+}
+
+GithubProjects.ShowRepository = function(user, elementId) {
+	var githubUrl = "http://github.com/api/v2/json/";
+	var reposUrl = githubUrl + "repos/show/" + user;
+	$.ajax({
+		url: reposUrl,
+		type: "GET",
+		dataType: "jsonp",
+		success: function(msg) {
+			var proj = $("#" + elementId);
+			var content = "<table>";
+			$.each(msg.repositories, function(i, val) {
+				var githubUserUrl = "http://github.com/" + user + "/";
+				var img = githubUserUrl + val.name + "/raw/master/.web/thumbnail.png";
+				var url = githubUserUrl + val.name + "/tree/master";
+				var item = "<tr><td><a href='__URL__'><img border='0' style='max-width:200px; max-height:200px; visibility:hidden;' src='__IMG__' onload='GithubProjects.showImage(this)'/></a></td><td>__NAME__</td><td>__DESCRIPTION__</td></tr>".replace("__NAME__", val.name).replace("__DESCRIPTION__", val.description).replace(/__IMG__/g, img).replace("__URL__", url);
+				content += item;
+			});
+			content += "</table>"
+			proj[0].innerHTML = content;
+		}
+	})
+};
