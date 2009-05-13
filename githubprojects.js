4a9cfaa6397a096ee9229b92a4788a689ad50b1e
Koushik Dutta <koush@Koushik-MacBook.local>
2009-05-12 18:38:55 -0700

    adjust colors

--- githubprojects.js
+++ githubprojects.js
@@ -14,12 +14,15 @@ GithubProjects.ShowRepository = function(user, elementId) {
 		success: function(msg) {
 			var proj = $("#" + elementId);
 			var content = "<table>";
+			var background = false;
 			$.each(msg.repositories, function(i, val) {
 				var githubUserUrl = "http://github.com/" + user + "/";
 				var img = githubUserUrl + val.name + "/raw/master/.web/thumbnail.png";
 				var url = githubUserUrl + val.name + "/tree/master";
-				var item = "<tr><td width='200'>__NAME__</td><td><a href='__URL__'><img border='0' style='max-width:200px; max-height:200px; visibility:hidden;' src='__IMG__' onload='GithubProjects.showImage(this)'/></a></td><td>__DESCRIPTION__</td></tr>".replace("__NAME__", val.name).replace("__DESCRIPTION__", val.description).replace(/__IMG__/g, img).replace("__URL__", url);
+				var bgcolor = background ? "bgcolor='#EFEFEF'" : "";
+				var item = "<tr align='center' __BGCOLOR__><td width='200'>__NAME__</td><td><a href='__URL__'><img border='0' style='max-width:200px; max-height:200px; visibility:hidden;' src='__IMG__' onload='GithubProjects.showImage(this)'/></a></td><td>__DESCRIPTION__</td></tr>".replace("__NAME__", val.name).replace("__DESCRIPTION__", val.description).replace(/__IMG__/g, img).replace("__URL__", url).replace("__BGCOLOR__", bgcolor);
 				content += item;
+				background = !background;
 			});
 			content += "</table>"
 			proj[0].innerHTML = content;
